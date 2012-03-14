3f49a190e8f7672b33705d859a01f63fdcba4a4f
Stephen Brown II <stbrown@jhuccp.org>
2012-03-14 14:04:42 -0400

    Added --format|-l option, to allow for parsing IIS formatted log files.
    Also changed parsing to use hashes for easier access of all variables.
    Unfortunately, this nearly doubles the time requred to parse the log.

--- ip-count.pl
+++ ip-count.pl
@@ -6,15 +6,16 @@ use Getopt::Long;
 use File::Basename;
 
 my %o=();
-my ($file, $pattern, $min, $max);
-GetOptions( \%o, 'help|h|?','file|f=s','pattern|p=s','min=i','max=i'); 
+my ($file, $pattern, $format, $min, $max);
+GetOptions( \%o, 'help|h|?','file|f=s','pattern|p=s','format|l=s','min=i','max=i');
 
 if (defined $o{help}) {
-    print "Usage: " . basename($0) . " --file|-f <filename> [--pattern|-p <pattern>] [--min <min>] [--max <max>]\n\n";
+    print "Usage: " . basename($0) . " --file|-f <filename> [--pattern|-p <pattern>] [--format|-l <format>] [--min <min>] [--max <max>]\n\n";
     print "       --help|-h|-? prints this message and exits.\n\n";
     print "       <filename>   is required, lists the file you wish to pull the IP hit counts from.\n\n";
     print "       <pattern>    is optional, but limits the results to only lines containing <pattern>,\n";
     print "                    a regular expression. Useful to limit by time like: '02/Aug/2011:21:[12]'.\n\n";
+    print "       <format>     is optional, specifying the format of the logfile (common, combined, or IIS).\n\n";
     print "       <min> and <max> can be used individually or together to narrow the results further,\n";
     print "                    printing only IPs that have at least <min> hits or no more than <max> hits.\n\n";
     print "\n      *Note: For larger log files (>100MB) or when your server is currently under high load,\n";
@@ -31,6 +32,8 @@ if (defined $o{file}) {
 }
 
 $pattern = $o{pattern} if defined $o{pattern};
+
+$format = (defined $o{format}) ? $o{format} : 'common';
 
 if (defined $o{min}) {
     $min = $o{min};
@@ -58,8 +61,16 @@ while (<FILE>){
     if (defined $pattern) {
         next unless m#$pattern#g;
     }
-    my ($ip) = split;
-    $IPs{$ip}++;
+    if (uc $format eq 'IIS') {
+        chomp;
+        next if /^#/; # Skip commented headers in IIS logs
+        my %r = &parse_iis_line_to_hash($_);
+        $IPs{$r{c_ip}}++;
+    } else {
+        chomp;
+        my %r = &parse_apache_line_to_hash($_);
+        $IPs{$r{client}}++;
+    }
 
     $total_ips++;
 }
@@ -85,4 +96,59 @@ sub find_ip_hostname() {
         my $name = gethostbyaddr(inet_aton($ip),AF_INET);
         return defined($name) ? $name : 'NO PTR RECORD';
     }
+}
+
+sub parse_apache_line_to_hash() {
+    # Adapted from ApacheLog::Parser, which only does 'combined'
+    my ($line) = @_;
+    my @fields = qw( client ruser login dtime request file params proto code bytes refer agent );
+    my $common_rx = qr/^
+      ([^ ]+)\ +([^ ]+)\ +([^\[]+)\ +            # client, ruser, login
+      \[([^\]]+)\]\ +                            # date
+      "(.*)"\ +(\d+)\ +(\d+|-)                   # req, code, bytes
+    $/x;
+    my $combined_rx = qr/^
+      ([^ ]+)\ +([^ ]+)\ +([^\[]+)\ +            # client, ruser, login
+      \[([^\]]+)\]\ +                            # date
+      "(.*)"\ +(\d+)\ +(\d+|-)\ +                # req, code, bytes
+      "(.*)"\ +"(.*)"                            # refer, agent
+    $/x;
+    my %log;
+    my $req;
+
+    if ($line =~ $common_rx) {
+        ($log{client}, $log{ruser}, $log{login}, $log{dtime}, $req, $log{code}, $log{bytes}) =
+        ($1, $2, $3, $4, $5, $6, $7);
+    } elsif ($line =~ $combined_rx) {
+        ($log{client}, $log{ruser}, $log{login}, $log{dtime}, $req, $log{code}, $log{bytes}, $log{refer}, $log{agent}) =
+        ($1, $2, $3, $4, $5, $6, $7, $8, $9);
+    } else {
+        die "Failed to parse line: $line\n";
+    }
+
+    $log{code} or die "no code in $line";
+
+    $req =~ s/^(?:([A-Z]+) +)?//;
+    $log{request} = $1 || ''; # ouch, a non-request (telnet) hack
+    # just tear this off the end
+    $log{proto} = ($req =~ s{ +(HTTP/\d+\.\d+)$}{}) ? $1 : '';
+
+    ($log{file},$log{params}) = split(/\?/, $req, 2);
+    defined($log{$_}) or $log{$_} = '' for('file', 'params');
+    $log{params} =~ s/\\"/"/g;
+
+    (!defined($log{$_}) or ($log{$_} eq '-')) and $log{$_} = ''
+      for('ruser', 'login', 'request', 'code', 'refer', 'agent');
+    $log{bytes} = 0 if($log{bytes} eq '-');
+
+    return %log;
+}
+
+sub parse_iis_line_to_hash() {
+   my ($line) = @_;
+   my %log;
+   my @fields = qw(date time s_sitename s_ip cs_method cs_uri_stem cs_uri_query server_port
+                   cs_username c_ip c_ua sc_status sc_substatus sc_win32_status);
+   @log{@fields} = split(/ /, $line);
+   return %log;
 }
